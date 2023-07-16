e6f24067b41354642a6c8c9033e5336ce0de8247
Mohamad Ghaith Alzin <ghaithalzein05@gmail.com>
2023-07-16 13:38:31 +0900

    add api to fetch blogs [

--- models/User.js
+++ models/User.js
@@ -14,6 +14,10 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  articles: {
+    type: [mongoose.Schema.Types.ObjectId],
+    default: [],
+  },
 });
 
 const User = mongoose.model("User", userSchema);
