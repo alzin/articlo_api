b26c249bbb56f6a8df094e2b5ac194ab5c24d1d6
Mohamad Ghaith Alzin <ghaithalzein05@gmail.com>
2023-07-28 18:49:58 +0900

    Add some Express config

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,26 @@
+import { log } from 'console';
 import express, { Request, Response } from 'express';
+import bodyParser from "body-parser";
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.get('/', (req: Request, res: Response) => {
-  res.send('This is the articlo app api');
+  res.send('Hello to articlo backend app!!!');
 });
 
 app.listen(PORT, () => {
