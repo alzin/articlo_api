458c28d64bae3eaf5e954f0d825b092a27474afb
Mohamad Ghaith Alzin <ghaithalzein05@gmail.com>
2023-07-28 14:42:12 +0900

    start with simple Exprress app

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log('Hello world!')
+import express, { Request, Response } from 'express';
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('This is the articlo app api');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
