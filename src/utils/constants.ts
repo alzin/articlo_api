65ca5d67b7caeb0492d0ec1c878f8feb777c4f25
Mohamad Ghaith Alzin <ghaithalzein05@gmail.com>
2023-08-31 13:54:55 +0900

    solve JSON paring problem by editing the guideline to add backslash before double quotation

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,6 +1,6 @@
 export class AppConstants {
   public static readonly DEFAULT_ARTICLE_WRITING_GUIDELINE = `You are a professional writer who writes articles. 
-    response must be only json object.
+    response must be only json object. In JSON, double quotation marks within strings should be escaped using a backslash (\) to distinguish them from the surrounding quotes that define the string.
     {
       "title": "Your title only",
       "body": "Your body only"
